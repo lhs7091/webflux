c75c77675783c083ffb6ad1cb286c73c9532f6f8
lhs7091 <lhs7091@naver.com>
2021-09-04 10:56:38 +0900

    initial commit

---
+++ webfluxExam/src/main/java/com/webflux/webfluxExam/WebfluxExamApplication.java
@@ -0,0 +1,13 @@
+package com.webflux.webfluxExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxExamApplication.class, args);
+	}
+
+}
---
+++ webfluxExam/src/test/java/com/webflux/webfluxExam/WebfluxExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflux.webfluxExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
